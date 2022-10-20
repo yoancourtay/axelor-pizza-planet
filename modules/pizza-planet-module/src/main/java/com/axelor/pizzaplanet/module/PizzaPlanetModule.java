03f39b0b8b02a705d37e048d7fb046d4997e1d76
yoancourtay <yoan.courtay-bonneau@outlook.com>
2022-10-20 12:54:18 +0200

    fix

--- modules/pizza-planet-module/src/main/java/com/axelor/pizzaplanet/module/PizzaPlanetModule.java
+++ modules/pizza-planet-module/src/main/java/com/axelor/pizzaplanet/module/PizzaPlanetModule.java
@@ -1,4 +1,4 @@
-package com.axelor.pizzaplanet;
+package com.axelor.pizzaplanet.module;
 
 import com.axelor.app.AxelorModule;
 import com.axelor.pizza.db.repo.PizzaRepository;
@@ -6,7 +6,7 @@ import com.axelor.pizzaplanet.repository.PizzaRepositoryExtended;
 import com.axelor.pizzaplanet.service.PizzaDoSomethingService;
 import com.axelor.pizzaplanet.service.PizzaDoSomethingServiceImpl;
 
-public class PizzaModule extends AxelorModule {
+public class PizzaPlanetModule extends AxelorModule {
     @Override
     protected void configure(){
         super.configure();
