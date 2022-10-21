766b7089bd5b7a85df1a4e1754f157309989d1db
yoancourtay <yoan.courtay-bonneau@outlook.com>
2022-10-21 16:01:31 +0200

    Removed unused imports

--- modules/meteo-france-module/src/main/java/com/axelor/meteofrance/web/MeteoController.java
+++ modules/meteo-france-module/src/main/java/com/axelor/meteofrance/web/MeteoController.java
@@ -1,8 +1,5 @@
 package com.axelor.meteofrance.web;
 
-import com.axelor.inject.Beans;
-import com.axelor.meteo.db.Commune;
-import com.axelor.meteofrance.service.ApiRequestService;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 
